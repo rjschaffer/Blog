347e61be0e990296a2c6575b2fe84ccbf6dabccc
rjschaffer <72329632+rjschaffer@users.noreply.github.com>
2021-09-25 16:05:30 -0400

    Initial commit

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ class Comment(db.Model):
     comment_author = relationship("User", back_populates="comments")
     post = relationship("BlogPost", back_populates="comments")
 
-# db.create_all()
+db.create_all()
 
 
 def admin_only(f):
